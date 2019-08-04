5896a40e11daedfba14085eb96d3d6b6fe18a146
Aric Parkinson <aric.parkinson@gmail.com>
2019-08-03 20:44:40 -0600

    Fix up ssh key generation

--- osx/_setup_ssh.sh
+++ osx/_setup_ssh.sh
@@ -1,8 +1,15 @@
-if [[ ! -f ${HOME}/.ssh/id_rsa ]]; then
+SSH_FILE=${HOME}/.ssh/id_rsa
+SSH_CONFIG_FILE=${HOME}/.ssh/config
+
+if [[ ! -f ${SSH_FILE} ]]; then
     echo "Creating ssh key. Please wait for further instructions..."
-    ssh-keygen -t rsa -b 4096
-    ssh-add -K ${HOME}/.ssh/id_rsa
-    pbcopy < ${HOME}/.ssh/id_rsa.pub
+    ssh-keygen -t rsa -b 4096 -f ${SSH_FILE}
+
+    eval "$(ssh-agent -s)"
+    cp ssh-config ${SSH_CONFIG_FILE}
+    ssh-add -K ${SSH_FILE}
+
+    pbcopy < "${SSH_FILE}.pub"
     echo "Your public key has been copied to the clipboard."
     echo "Please add it to Github!"
     read -p "Don't worry, I'll wait..." UNUSED
