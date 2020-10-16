bd2f04f50465e4c87a19fd9ae359a26867b8ccfa
Aric Parkinson <aric.parkinson@gmail.com>
2020-10-15 22:22:56 -0600

    Update git configs to use "main" as default branch name
     - This corresponds with GitHub's new recommendations. I'll be updating
       the branch for this repo as well, once I have a chance to figure out
       which of these pending changes that have been sitting around are
       valid and which aren't, haha

--- osx/_setup_git.sh
+++ osx/_setup_git.sh
@@ -8,6 +8,10 @@ if [[ -z `git config --list | grep user.name` ]]; then
     git config --global user.name "Aric Parkinson"
 fi
 
+if [[ -z `git config --list | grep init.defaultBranch` ]]; then
+    git config --global init.defaultBranch "main"
+fi
+
 if [[ ! -f /usr/local/etc/bash_completion ]]; then
     brew install git bash-completion
 fi
