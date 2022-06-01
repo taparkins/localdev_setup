ee1f96b07cc0c0e62c13da6a3432a267611aac8c
Aric Parkinson <aric.parkinson@datadoghq.com>
2022-06-01 12:52:27 -0600

    Some quick fixes to the _setup_rcfiles commit

--- osx/_setup_rcfiles.sh
+++ osx/_setup_rcfiles.sh
@@ -4,6 +4,7 @@ if [[ -z "$LOCALDEV_ZSHRC_VERSION" ]]; then
     cat .zshrc >> ~/.zshrc
 fi
 if [[ "$LOCALDEV_ZSHRC_VERSION" != "$CURRENT_ZSHRC_VERSION" ]]; then
+    cp .dircolors ~/.dircolors
     cp .external_rc ~/.external_rc
     source ~/.external_rc
 fi
