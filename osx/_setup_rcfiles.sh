6e0e06fdeaa4caefcd4021e42c3773be923b9755
Talia Parkinson <talia.parkinson@gmail.com>
2023-05-22 16:31:14 -0600

    Install rcfile dependencies

--- osx/_setup_rcfiles.sh
+++ osx/_setup_rcfiles.sh
@@ -3,6 +3,16 @@ CURRENT_ZSHRC_VERSION=1
 if [[ -z "$LOCALDEV_ZSHRC_VERSION" ]]; then
     cat .zshrc >> ~/.zshrc
 fi
+
+HAS_COREUTILS=$(brew ls --versions coreutils)
+if [[ -z ${HAS_COREUTILS} ]]; then
+    brew install coreutils
+fi
+
+if [[ ! -d "~/.dircolors" ]]; then
+    git clone https://github.com/gibbling666/dircolors.git ~/.dircolors
+fi
+
 if [[ "$LOCALDEV_ZSHRC_VERSION" != "$CURRENT_ZSHRC_VERSION" ]]; then
     cp .dircolors ~/.dircolors
     cp .external_rc ~/.external_rc
