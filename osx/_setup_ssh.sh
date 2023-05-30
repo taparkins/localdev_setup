5fdedbf66d265f15e96545d4aa9eeecf95939ccf
Talia Parkinson <talia.parkinson@datadoghq.com>
2023-05-30 14:52:54 -0600

    Github doesn't accept RSA keys anymore, I guess, so update to use ed25519 instead

--- osx/_setup_ssh.sh
+++ osx/_setup_ssh.sh
@@ -1,16 +1,23 @@
-SSH_FILE=${HOME}/.ssh/id_rsa
+OLD_SSH_FILE=${HOME}/.ssh/id_rsa
+NEW_SSH_FILE=${HOME}/.ssh/id_localdev
 SSH_CONFIG_FILE=${HOME}/.ssh/config
 
-if [[ ! -f ${SSH_FILE} ]]; then
-    echo "Creating ssh key. Please wait for further instructions..."
-    ssh-keygen -t rsa -b 4096 -f ${SSH_FILE}
-
-    eval "$(ssh-agent -s)"
-    cp ssh-config ${SSH_CONFIG_FILE}
-    ssh-add --apple-use-keychain ${SSH_FILE}
-
-    pbcopy < "${SSH_FILE}.pub"
-    echo "Your public key has been copied to the clipboard."
-    echo "Please add it to Github!"
-    read -p "Don't worry, I'll wait..." UNUSED
+if [[ -f ${OLD_SSH_FILE} ]]; then
+    echo "Legacy SSH key already exists. Skipping..."
+    exit 0
+elif [[ -f ${NEW_SSH_FILE} ]]; then
+    echo "SSH key already exists. Skipping..."
+    exit 0
 fi
+
+echo "Creating ssh key. Please wait for further instructions..."
+ssh-keygen -t ed25519 -b 4096 -f ${SSH_FILE}
+
+eval "$(ssh-agent -s)"
+cp ssh-config ${SSH_CONFIG_FILE}
+ssh-add --apple-use-keychain ${SSH_FILE}
+
+pbcopy < "${SSH_FILE}.pub"
+echo "Your public key has been copied to the clipboard."
+echo "Please add it to Github!"
+read -p "Don't worry, I'll wait..." UNUSED
