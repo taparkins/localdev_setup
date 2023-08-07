2818f3271222c7454df7fb0e9de02be8c90af16e
Talia Parkinson <talia.parkinson@datadoghq.com>
2023-08-07 14:35:59 -0600

    Checkout vimrc from specified commit, rather than most recent
     - This guy added a BUNCH of shit in the last year or so, and the
       plugins have gotten borderline unusable as a consequence. PLUS it
       installed CoPilot without my realizing it, which is shady as fuck
       tbph. This is doing a shallow clone from a specified hash until I get
       around to forking the repo to avoid this happening in the future.

--- osx/_setup_vim.sh
+++ osx/_setup_vim.sh
@@ -20,8 +20,21 @@ fi
 
 # Install Vim Awesome + custom configs
 if [[ ! -d ~/.vim_runtime ]]; then
-    git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+    mkdir ~/.vim_runtime
+    pushd ~/.vim_runtime
+
+    # we're looking to do a shallow-clone based on a specific commit
+    # you cannot directly clone from a specified SHA, so we're working
+    # around it based on this suggestion:
+    # https://stackoverflow.com/a/43136160
+    git init
+    git remote add origin https://github.com/amix/vimrc.git
+    # Oct 11, 2021
+    git fetch --depth 1 origin 92c794cc2b5206f199ff6e2e2c4840a3d49c0ea8
+    git checkout FETCH_HEAD
     sh ~/.vim_runtime/install_awesome_vimrc.sh
+
+    popd
 fi
 
 # Copy over custom vimrc
